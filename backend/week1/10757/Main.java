a3f31c77bf2233b5c4ba3dc6660393311ecef1a6
kimjunyoung <me@kimjunyoung.com>
2023-04-25 18:54:53 +0900

    1주차 과제 진행중

---
+++ backend/week1/10757/Main.java
@@ -0,0 +1,15 @@
+// 입력 예시 값이 long 자료형이 표현할 수 있는 값을 넘어섬
+// long 자료형보다 큰 값을 처리하기 위해 BigInteger 클래스 호출
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        BigInteger A = scanner.nextBigInteger();
+        BigInteger B = scanner.nextBigInteger();
+
+        // BigInteger는 일반적인 연산자를 사용할 수 없으므로 add 메소드를 이용하여 계산
+        System.out.println(A.add(B));
+    }
+}
---
+++ backend/week1/10869/Main.java
@@ -0,0 +1,17 @@
+// 입력값을 받기 위해 Scanner 클래스 호출
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+    }
+}
